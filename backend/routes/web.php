d17ead460c2713e519e4d77e8d414c6e691d57b7
Willson0 <logvinovnikita3@gmail.com>
2026-03-18 01:06:52 +0500

    11

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -6,4 +6,5 @@ use App\Http\Middleware\CheckTelegram;
 use Illuminate\Support\Facades\Route;
 
 Route::post("/payment/webhook_jgkgfhs7430jdfsd", [PaymentController::class, 'webhook']);
-Route::post("/md/{hash}", [ViewerController::class, 'get'])->middleware(CheckTelegram::class);
+Route::post("/md/{hash}", [ViewerController::class, 'get']);
+//    ->middleware(CheckTelegram::class);
